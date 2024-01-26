aa5dd54b6b0057c07a2dbc627f1bbf69fdcc0a63
Higor Zica <higorbhe@gmail.com>
2024-01-26 18:03:50 +0100

    atz images directory

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TFF2RMV3.mjs";import"./chunk-RBIBRJHW.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-ENGPFZLZ.mjs";import"./chunk-RBIBRJHW.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
